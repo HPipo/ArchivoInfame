cdcd11141eb4d0484d73267645d7ea854016bd11
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-03 15:20:54 -0300

    testing api
    added config to fetch

--- js/wikiapi.js
+++ js/wikiapi.js
@@ -2,6 +2,6 @@ let title = 'José_Félix_Uriburu'
 
 let wikiUri = `https://es.wikipedia.org/wiki/api.php?action=query&prop=revisions&titles=José_Félix_Uriburu&rvslots=*&rvprop=content&formatversion=2`
 
-fetch(wikiUri)
+fetch(wikiUri, {'mode': 'no-cors', 'headers': {'Access-Control-Allow-Origin': '*'}})
 .then((resp) => {resp.json()})
 .then((data) => {console.log(data)})

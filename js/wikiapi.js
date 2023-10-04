978f59308c368e9952078399d5dbc134a219d104
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-04 13:32:26 -0300

    updated api

--- js/wikiapi.js
+++ js/wikiapi.js
@@ -1,6 +1,6 @@
 let uriLink = 'https://es.wikipedia.org/w/api.php?action=query&prop=revisions&titles=José_Félix_Uriburu&rvslots=*&rvprop=content&formatversion=2&format=json'
 
-fetch(`https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=Pet_door&rvslots=*&rvprop=content&formatversion=2&format=json`, {
+fetch(uriLink, {
     'mode': 'no-cors',
     'method': 'get',
     'headers': {'Access-Control-Allow-Origin' : "*"}})

32c6a657ec28b1afb8a21e94df14f8dab9e7bef8
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-03 22:36:06 -0300

    modifying api

--- js/wikiapi.js
+++ js/wikiapi.js
@@ -1,7 +1,7 @@
 let title = 'José_Félix_Uriburu'
 
-let wikiUri = `https://es.wikipedia.org/wiki/api.php?action=query&prop=revisions&titles=José_Félix_Uriburu&rvslots=*&rvprop=content&formatversion=2`
+let wikiUri = `https://es.wikipedia.org/wiki/api.php?action=query&prop=revisions&titles=Pet_door&rvslots=*&rvprop=content&formatversion=2`
 
-fetch(wikiUri, {'mode': 'no-cors', 'headers': {'Access-Control-Allow-Origin': '*'}})
-.then((resp) => {resp.json()})
+fetch(wikiUri, {'mode': 'no-cors'})
+.then((resp) => {return resp.json()})
 .then((data) => {console.log(data)})

ed412879d5d78fde8e4976e5ec23046394afb5f3
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-25 19:33:58 -0300

    general update
    added json
    added fetch

--- js/fetch.js
+++ js/fetch.js
@@ -2,33 +2,51 @@ let cardPres = document.getElementById("cardPresidencial")
 
 fetch("../../card.json")
     .then((res) =>  res.json())
-    .then((obj) => {console.log(obj)})
     .then((data) =>{
         if (window.location.href.includes("uriburu")) {
-            for (let element in data) {
-                cardPres.innerHTML += `
-                <p class="cardPresElemento"></p>
-                <img class="cardPresElemento" id="presImg" src="" alt="">
+            cardPres.innerHTML +=
+                `<p class="cardPresElemento">${data[0].presidente.uriburu[0].nombre}</p>
+                <img class="cardPresElemento" id="presImg" src="${data[0].presidente.uriburu[0].img}" alt="">
                 <div class="cardPresElemento">
-                    <p></p>
-                    <p></p>
+                    <p>${data[0].presidente.uriburu[0].titulo}</p>
+                    <p>${data[0].presidente.uriburu[0].duracion}</p>
                 </div>
-                <p class="cardPresElemento"></p>
-                <p class="cardPresElemento"></p>
+                <p class="cardPresElemento">${data[0].presidente.uriburu[0].nacimiento}</p>
+                <p class="cardPresElemento">${data[0].presidente.uriburu[0].fallecimiento}</p>
                 `
-            }
+        }else if (window.location.href.includes("castillo")) {
+            cardPres.innerHTML +=
+                `<p class="cardPresElemento">${data[0].presidente.castillo[0].nombre}</p>
+                <img class="cardPresElemento" id="presImg" src="${data[0].presidente.castillo[0].img}" alt="">
+                <div class="cardPresElemento">
+                    <p>${data[0].presidente.castillo[0].titulo}</p>
+                    <p>${data[0].presidente.castillo[0].duracion}</p>
+                </div>
+                <p class="cardPresElemento">${data[0].presidente.castillo[0].nacimiento}</p>
+                <p class="cardPresElemento">${data[0].presidente.castillo[0].fallecimiento}</p>
+                `
+        }else if (window.location.href.includes("justo")) {
+            cardPres.innerHTML +=
+                `<p class="cardPresElemento">${data[0].presidente.justo[0].nombre}</p>
+                <img class="cardPresElemento" id="presImg" src="${data[0].presidente.justo[0].img}" alt="">
+                <div class="cardPresElemento">
+                    <p>${data[0].presidente.justo[0].titulo}</p>
+                    <p>${data[0].presidente.justo[0].duracion}</p>
+                </div>
+                <p class="cardPresElemento">${data[0].presidente.justo[0].nacimiento}</p>
+                <p class="cardPresElemento">${data[0].presidente.justo[0].fallecimiento}</p>
+                `
         }else {
-            cardPres.innerHTML += data.forEach(element => {
-                `<p class="cardPresElemento">${castillo.nombre}</p>
-                <img class="cardPresElemento" id="presImg" src="${castillo.img}" alt="">
+            cardPres.innerHTML +=
+                `<p class="cardPresElemento">${data[0].presidente.ortiz[0].nombre}</p>
+                <img class="cardPresElemento" id="presImg" src="${data[0].presidente.ortiz[0].img}" alt="">
                 <div class="cardPresElemento">
-                    <p>${castillo.titulo}</p>
-                    <p>${castillo.duracion}</p>
+                    <p>${data[0].presidente.ortiz[0].titulo}</p>
+                    <p>${data[0].presidente.ortiz[0].duracion}</p>
                 </div>
-                <p class="cardPresElemento">${castillo.nacimiento}</p>
-                <p class="cardPresElemento">${castillo.fallecimiento}</p>
+                <p class="cardPresElemento">${data[0].presidente.ortiz[0].nacimiento}</p>
+                <p class="cardPresElemento">${data[0].presidente.ortiz[0].fallecimiento}</p>
                 `
-            });
         }
     })
     .catch("Error en JSON")

790ca8ab7b6a7b5c6cf06402f04e0cc2143bcf27
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-25 18:26:29 -0300

    updated fetch

--- js/fetch.js
+++ js/fetch.js
@@ -3,7 +3,7 @@ let cardPres = document.getElementById("cardPresidencial")
 fetch("../../card.json")
     .then((res) =>  res.json())
     .then((obj) => {console.log(obj)})
-/*     .then((data) =>{
+    .then((data) =>{
         if (window.location.href.includes("uriburu")) {
             for (let element in data.presidente) {
                 cardPres.innerHTML += `
@@ -14,9 +14,20 @@ fetch("../../card.json")
                     <p></p>
                 </div>
                 <p class="cardPresElemento"></p>
-                <p class="cardPresElemento"></p
+                <p class="cardPresElemento"></p>
                 `
             }
-        }
-    }) */
+        }else {
+            for (let element in data.presidente) {
+                cardPres.innerHTML += `
+                <p class="cardPresElemento">${element.castillo[0]}</p>
+                <img class="cardPresElemento" id="presImg" src="${element.castillo[1]}" alt="">
+                <div class="cardPresElemento">
+                    <p>${element.castillo[2]}</p>
+                    <p>${element.castillo[3]}</p>
+                </div>
+                <p class="cardPresElemento">${element.castillo[4]}</p>
+                <p class="cardPresElemento">${element.castillo[5]}</p>`
+        }}
+    })
     .catch("Error en JSON")

4cfe78778afe4353ab0ef1b508f56086ada567ac
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-25 00:14:29 -0300

    updated fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,3 +1,10 @@
 fetch("../../card.json")
-    .then(data => console.log(data))
-    .then(res => res.json())
+    .then((res) => res.json())
+    .then((data) =>{
+        if (window.location.href.includes("uriburu")) {
+            for (element in data.presidente) {
+                console.log(data)
+            }
+        }
+    })
+    .catch("Error en JSON")

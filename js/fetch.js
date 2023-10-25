4424e0f4190bb05c2757a701fbe93172d2863bd9
HPipo <FilippoDapernoUNR@outlook.com>
2023-10-25 18:12:58 -0300

    updated fetch

--- js/fetch.js
+++ js/fetch.js
@@ -1,10 +1,22 @@
+let cardPres = document.getElementById("cardPresidencial")
+
 fetch("../../card.json")
-    .then((res) => res.json())
-    .then((data) =>{
+    .then((res) =>  res.json())
+    .then((obj) => {console.log(obj)})
+/*     .then((data) =>{
         if (window.location.href.includes("uriburu")) {
             for (let element in data.presidente) {
-                console.log(element)
+                cardPres.innerHTML += `
+                <p class="cardPresElemento"></p>
+                <img class="cardPresElemento" id="presImg" src="" alt="">
+                <div class="cardPresElemento">
+                    <p></p>
+                    <p></p>
+                </div>
+                <p class="cardPresElemento"></p>
+                <p class="cardPresElemento"></p
+                `
             }
         }
-    })
+    }) */
     .catch("Error en JSON")
